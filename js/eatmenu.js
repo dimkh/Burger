499ddd39b77e803925ebd4a93b811626dbcd58a5
dimkh <dhaldeev@gmail.com>
2020-03-26 00:58:05 +0500

    Исправлены замечания наставника по ДЗ-3

---
+++ js/eatmenu.js
@@ -0,0 +1,30 @@
+(function eatMenu() {
+
+  const eatMenuList = document.querySelector('.eat-menu__acco');
+  let eatMenuActive;
+
+  console.log(eatMenuActive);
+
+  eatMenuList.addEventListener('click', function(e) {
+    
+    // Клик по ссылке обновляет страницу - нам это не нужно
+    e.preventDefault();
+    
+    // Клик по активному классу - деактивируем (закрываем меню)
+    if (e.path[1].classList.contains('eat-menu__acco-item-active')) {
+      e.path[1].classList.remove('eat-menu__acco-item-active');
+    }
+    else {
+      // Удаляем активный класс, если он есть у других
+      eatMenuActive = document.querySelector('.eat-menu__acco-item-active');
+      if (!(eatMenuActive === null)) {
+        eatMenuActive.classList.remove('eat-menu__acco-item-active');
+      }
+
+      // Добавляем активный класс лишке, в которой лежит кликнутая ссылка
+      e.path[1].classList.add('eat-menu__acco-item-active');
+    }
+
+  });
+
+}) ();
--- js/openmenu.js
+++ js/openmenu.js
@@ -6,15 +6,15 @@
   menuBtn.addEventListener('click', function() {
     menuBtn.classList.toggle('hamburger-closed');
     modalMenu.classList.toggle('modal-menu--active');
-  })
+  });
 
   modalMenu.addEventListener('click', function(e) {
 
     if (e.target.classList.contains('modal-menu__link')) {
       menuBtn.classList.toggle('hamburger-closed');
       modalMenu.classList.toggle('modal-menu--active');
-    };
+    }
 
-  })
+  });
 
 }) ();
